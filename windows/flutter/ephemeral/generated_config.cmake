c9126119c14480397efe6385a63ff8606779e85c
Subrina Meem <127532033+SubrinaMeem@users.noreply.github.com>
2024-09-03 21:24:55 +0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Programs\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\Mobile App Development\\foundit" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Programs\\flutter"
+  "PROJECT_DIR=F:\\Mobile App Development\\foundit"
+  "FLUTTER_ROOT=E:\\Programs\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\Mobile App Development\\foundit\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\Mobile App Development\\foundit"
+  "FLUTTER_TARGET=F:\\Mobile App Development\\foundit\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNWMyNGY1MzhkMDVhYWY2NmY3OTcyZmIyMzk1OWQ4Y2FmYjlmOTVhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\Mobile App Development\\foundit\\.dart_tool\\package_config.json"
+)
